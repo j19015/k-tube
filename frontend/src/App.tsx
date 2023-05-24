89008cc0a060e3c20c99231e42ce7cabfbef088c
j19015 <j19015@sangi.jp>
2023-05-24 16:35:23 +0900

    docker setting ver1.4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      aaaaaaa
+      bbbbbbb
+      aaaaaaa
+      sssssss
+      aaaaaaa
+      aaaaaaa
+      aaaaaaa
+      sssssss
+      aaaaaaa
+      aaaaaaa
+      sssssss
+      ｄｄｄｄｄ
+      sssssss
+      sssssss
+      ssssss
+      aaaaaaa
+      aaaaaaa
+      honda
     </div>
   );
 }
